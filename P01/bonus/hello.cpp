e72fb96f883c9cd93075ed497fafe2647f3706c1
MichaelBonnet <maabonnet@gmail.com>
2019-08-28 23:45:58 -0500

    “Homework

---
+++ P01/bonus/hello.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	string name;
+	cout << "Please enter your name: " << endl;
+	cin >> name;
+	cout << "Hello, " << name << "!" << endl;
+	return 0;
+}
