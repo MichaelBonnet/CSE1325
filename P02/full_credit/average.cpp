48bec227114d27165c381427cacd5497535b71ac
MichaelBonnet <maabonnet@gmail.com>
2019-09-16 21:58:07 -0500

    realizing I have no idea how to call my functions

--- P02/full_credit/average.cpp
+++ P02/full_credit/average.cpp
@@ -1,41 +1,32 @@
 #include "average.h"
 
-class Average
+Average::Average(): _sum{0}, _values{0} {};
+std::ostream& operator<<(ostream& ost, Average& average)
 {
-	private:
-		double _sum;
-		int _values;
-
-	public:
-		Average (double sum, int values);
+	if (average._values == 0)
+	{
+		{ost << "UNDEFINED";}
+	}
+	else
+	{
+		double average = (average._sum / average._values);
+		ost << average;
+	}
+	return ost;
+}
 		
-		std::ostream& operator <<(ostream& ost, Average& average)
-		{
-			if (average._values == 0)
-			{
-				{ost << "UNDEFINED";}
-			}
-			else
-			{
-				double average = (average._sum / average._values);
-				ost << average;
-			}
-			return ost;
-		}
-		
-		std::istream& operator >>(istream& ist, Average& average)
-		{
-			int inc = 1;
-			double val;
-			ist >> val;
-			average._sum += val;
-			average._values += inc;
-		}
+std::istream& operator>>(istream& ist, Average& average)
+{
+	int inc = 1;
+	double val;
+	ist >> val;
+	average._sum += val;
+	average._values += inc;
+}
 
-		Average& operator+=(double value)
-		{
-			int inc = 1;
-			_sum += value;
-			_values += inc;
-		}
+Average& operator+=(double value)
+{
+	int inc = 1;
+	_sum += value;
+	_values += inc;
 }
--- P02/full_credit/main.cpp
+++ P02/full_credit/main.cpp
@@ -7,9 +7,26 @@ int main(int argc, char const *argv[])
 {
 	int choice;
 
+	Average average(0, 0);
+
 	while (choice != 0)
 	{
+		cout << "    =========================";
+		cout << "    Merely Average Calculator";
+		cout << "    =========================";
+		cout << "The current average is " << average.operator<<();
+		cout << " ";
+		cout << "1 - Enter a new value";
+		cout << "2 - Auto enter a random value";
+		cout << "9 - Clear the calculator";
+		cout << "0 - Exit";
+		cout << " ";
+		cout << "Command?";
 		cin >> choice;
+		if (choice == 1)
+		{
+			average.operator>>
+		}
 	}
 	return 0;
 }
