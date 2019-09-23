b79f93ec59050cdbafb8d69948a7164fcef9975d
MichaelBonnet <maabonnet@gmail.com>
2019-09-23 16:20:39 -0500

    may have figured out delegation

--- P04/coach.cpp
+++ P04/coach.cpp
@@ -2,22 +2,15 @@
 #include "coach.h"
 #include "stock.h"
 
-// constructor, which sets _weight (via delegation to the base constructor) to the parameter.
 // need to figure out delegation to the base constructor.
-Coach::Coach(double weight)
-{
-	// code
-}
+Coach::Coach(double weight) : Stock{weight} { }
 
-// adds its parameters to _passengers. If the resulting number of passengers exceeds 120, throw an exception.
 void Coach::add_passengers(int passengers)
 {
 	_passengers += passengers;
 	if ( _passengers > 120 ) { cout << "Exception: Too many passengers (more than 120). " << endl; }
 }
 
-// overrides Stock::weight(), returning the empty weight of the carriage plus the weight of 
-// the passengers. Each passenger weighs 60 kilograms.
 virtual double Coach::weight()
 {
 	return _weight + (_passengers * 60);
--- P04/locomotive.cpp
+++ P04/locomotive.cpp
@@ -2,11 +2,9 @@
 #include "locomotive.h"
 #include "stock.h"
 
-// sets _power and (via delegation to the base constructor) _weight to the parameter values.
 // need to figure out delegation to the base constructor.
-Locomotive::Locomotive(double weight, double power) : _weight{weight}, _power{power} { }
+Locomotive::Locomotive(double weight, double power) : Stock{weight}, _power{power} { }
 
-// returns _power (i.e., a "getter").
 double Locomotive::power()
 {
 	return _power;
